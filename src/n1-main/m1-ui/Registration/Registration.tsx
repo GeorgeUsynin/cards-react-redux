b00dcbeae6841f2f96f7f2792d51d847f65c5dda
andreisemianiuk <semeniukandrei677@gmail.com>
2021-06-26 16:54:27 +0300

    layout for registration done

--- src/n1-main/m1-ui/Registration/Registration.tsx
+++ src/n1-main/m1-ui/Registration/Registration.tsx
@@ -3,9 +3,34 @@ import cls from './Registration.module.css'
 
 
 export const Registration = () => {
-    return (
-        <div className={cls.registrationContainer}>
-            <h1>Registration</h1>
+  return (
+    <div className={cls.registrationContainer}>
+      <div className={cls.content}>
+        <h2 className={cls.title}>IT-Incubator</h2>
+        <h2 className={cls.title}>Sign Up</h2>
+        <div className={cls.form}>
+          <label>
+            <div className={cls.inputTitle}>Email</div>
+            <input className={cls.input} type="email"/>
+          </label>
+          <label>
+            <div className={cls.inputTitle}>Password</div>
+            <input className={cls.input} type="password"/>
+          </label>
+          <label>
+            <div className={cls.inputTitle}>Confirm Password</div>
+            <input className={cls.input} type="password"/>
+          </label>
         </div>
-    )
+        <div className={cls.buttonsContainer}>
+          <div>
+            <button className={cls.cancelBtn}>Cancel</button>
+          </div>
+          <div>
+            <button className={cls.registerBtn}>Register</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
