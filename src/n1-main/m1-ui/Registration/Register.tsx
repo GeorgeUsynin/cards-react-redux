1400bcf5984a81c6a7b241f621dad12135c59dab
andreisemianiuk <semeniukandrei677@gmail.com>
2021-06-30 18:42:58 +0300

    refactored styles

--- src/n1-main/m1-ui/Registration/Register.tsx
+++ src/n1-main/m1-ui/Registration/Register.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 import cls from './Register.module.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { register, setRegisterError, setRegisterLoading } from '../../m2-bll/registerReducer'
 import { AppRootStateType } from '../../m2-bll/store'
 import preloader from '../common/preloader/Spinner-2.gif'
+import SuperInputText from '../common/SuperInput/SuperInputText'
 
 export const Register: React.FC = () => {
   const dispatch = useDispatch()
@@ -13,20 +14,44 @@ export const Register: React.FC = () => {
   let [email, setEmail] = useState<string>('')
   let [pass, setPass] = useState<string>('')
   let [confirmPass, setConfirmPass] = useState<string>('')
+  let [validateEmailError, setValidateError] = useState<string>('')
+  let [validatePasswordError, setValidatePasswordError] = useState<string>('')
   
   useEffect(() => {
     const error = null
     dispatch(setRegisterError(error))
   }, [dispatch])
   
+  const changeEmailHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.currentTarget.value
+    setEmail(value)
+    if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+      setValidateError('invalid email')
+    } else {
+      setValidateError('')
+    }
+  }
+  const changePasswordHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.currentTarget.value
+    setPass(value)
+    if (value.length < 8 || value.length > 15) {
+      setValidatePasswordError('invalid password length')
+    } else {
+      setValidatePasswordError('')
+    }
+  }
+  
   const cancelHandler = () => {
-    console.log('cancel')
+    setEmail('')
+    setPass('')
+    setConfirmPass('')
   }
   const registerHandler = () => {
     dispatch(setRegisterLoading(true))
     if (pass === confirmPass) {
       dispatch(register(email, pass))
     } else {
+      dispatch(setRegisterLoading(false))
       dispatch(setRegisterError('password don\'t match'))
     }
   }
@@ -39,32 +64,58 @@ export const Register: React.FC = () => {
         <div className={cls.form}>
           <label>
             <div className={cls.inputTitle}>Email</div>
-            <input className={cls.input} type="email" value={email}
-                   onChange={(e) => setEmail(e.currentTarget.value)}/>
+            <div className={cls.inputContainer}>
+              <SuperInputText
+                className={cls.input}
+                value={email}
+                type="email"
+                error={validateEmailError}
+                onChange={changeEmailHandler}
+              />
+            </div>
           </label>
           <label>
             <div className={cls.inputTitle}>Password</div>
-            <input className={cls.input} type="password" value={pass}
-                   onChange={(e) => setPass(e.currentTarget.value)}/>
+            <div className={cls.inputContainer}>
+              <SuperInputText
+                className={cls.input}
+                value={pass}
+                type="password"
+                error={validatePasswordError}
+                onChange={changePasswordHandler}
+              />
+            </div>
           </label>
           <label>
             <div className={cls.inputTitle}>Confirm Password</div>
-            <input className={cls.input} type="password" value={confirmPass}
-                   onChange={(e) => setConfirmPass(e.currentTarget.value)}/>
+            <div className={cls.inputContainer}>
+              <SuperInputText
+                className={cls.input}
+                value={confirmPass}
+                type="password"
+                onChange={(e) => setConfirmPass(e.currentTarget.value)}
+              />
+            </div>
           </label>
         </div>
         <div className={cls.info}>
           {
             isFetching ?
               <div>
-                <img className={cls.loader} src={preloader} alt='preloader'/>
+                <img className={cls.loader} src={preloader} alt="preloader"/>
               </div>
-              : error ? <div className={cls.error}>{error}</div> : null}
+              : error && <div className={cls.error}>{error}</div>}
         </div>
         <div className={cls.buttonsContainer}>
           <button className={cls.cancelBtn} onClick={cancelHandler}>Cancel
           </button>
-          <button className={cls.registerBtn} onClick={registerHandler}>Register</button>
+          <button
+            className={isFetching ? `${cls.registerBtn} ${cls.disable}` : cls.registerBtn}
+            disabled={isFetching}
+            onClick={registerHandler}
+          >
+            Register
+          </button>
         </div>
       </div>
     </div>
