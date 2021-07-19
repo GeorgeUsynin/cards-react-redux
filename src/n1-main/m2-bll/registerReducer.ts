dcbc5499d6b647be04b0bc52570c7638174d5d19
George Usynin <angiejo03@gmail.com>
2021-07-19 23:36:57 +0300

    refactor register reducer

--- src/n1-main/m2-bll/registerReducer.ts
+++ src/n1-main/m2-bll/registerReducer.ts
@@ -1,86 +1,56 @@
-import { ApiRegister } from '../m3-dal/apiRegister'
-import { AppThunkType } from './store'
-
-export const REGISTER_LOADING = 'REGISTER/LOADING'
-export const REGISTER_ERROR = 'REGISTER/ERROR'
-export const REGISTER_SUCCESS = 'REGISTER/SUCCESS'
+import {ApiRegister} from '../m3-dal/apiRegister'
+import {AppThunkType} from './store'
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 
 type InitialStateType = {
-  isRegistered: boolean,
-  error: string | null,
-  isFetching: boolean
+    isRegistered: boolean,
+    error: string | null,
+    isFetching: boolean
 }
 
-export const registerInitialState: InitialStateType = {
-  isRegistered: false,
-  error: null,
-  isFetching: false,
+export const initialState: InitialStateType = {
+    isRegistered: false,
+    error: null,
+    isFetching: false,
 }
 
-
-export type RegisterActions = ReturnType<typeof setRegisterSuccess
-  | typeof setRegisterError
-  | typeof setRegisterLoading>;
-
-export const setRegisterSuccess = (isRegistered: boolean) => {
-  return {
-    type: REGISTER_SUCCESS,
-    isRegistered,
-  } as const
-}
-
-export const setRegisterError = (error: string | null) => {
-  return {
-    type: REGISTER_ERROR,
-    error,
-  } as const
-}
-
-export const setRegisterLoading = (isFetching: boolean) => {
-  return {
-    type: REGISTER_LOADING,
-    isFetching,
-  } as const
-}
-
-export const registerReducer = (state = registerInitialState, action: RegisterActions): InitialStateType => {
-  switch (action.type) {
-    case REGISTER_SUCCESS: {
-      return {
-        ...state,
-        isRegistered: action.isRegistered,
-      }
+const slice = createSlice({
+    name: 'register',
+    initialState: initialState,
+    reducers: {
+        setRegisterSuccess(state, action: PayloadAction<boolean>) {
+            state.isRegistered = action.payload
+        },
+        setRegisterError(state, action: PayloadAction<string | null>) {
+            state.error = action.payload
+        },
+        setRegisterLoading(state, action: PayloadAction<boolean>) {
+            state.isFetching = action.payload
+        }
     }
-    case REGISTER_ERROR: {
-      return {
-        ...state,
-        error: action.error,
-      }
-    }
-    case REGISTER_LOADING: {
-      return {
-        ...state,
-        isFetching: action.isFetching,
-      }
-    }
-    default: {
-      return state
-    }
-  }
-}
+
+})
+
+//reducer
+export const registerReducer = slice.reducer
+
+//actions
+export const {setRegisterError, setRegisterLoading, setRegisterSuccess} = slice.actions
 
 
-export const register =
-  (email: string, password: string): AppThunkType => async (dispatch) => {
+export const register = (email: string, password: string): AppThunkType => async (dispatch) => {
     dispatch(setRegisterLoading(true))
     try {
-      await ApiRegister.register(email, password)
-      dispatch(setRegisterSuccess(true))
+        await ApiRegister.register(email, password)
+        dispatch(setRegisterSuccess(true))
     } catch (e) {
-      const error = e.response.data.error
-      dispatch(setRegisterError(error))
+        const error = e.response.data.error
+        dispatch(setRegisterError(error))
+    } finally {
+        dispatch(setRegisterLoading(false))
     }
-    finally {
-      dispatch(setRegisterLoading(false))
-    }
-  }
+}
+
+export type RegisterActions = ReturnType<typeof setRegisterSuccess
+    | typeof setRegisterError
+    | typeof setRegisterLoading>;
